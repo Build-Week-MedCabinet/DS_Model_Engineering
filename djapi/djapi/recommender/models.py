10aa68d3e799115f3c7d5b23513d760125d62fcf
Vincent Brandon <vincenta..brandon@gmail.com>
2019-09-22 16:55:49 +0000

    added custom models

--- djapi/djapi/recommender/models.py
+++ djapi/djapi/recommender/models.py
@@ -1,3 +1,61 @@
-from django.db import models
+from django.db.models import Model, DateTimeField, CharField, IntegerField, TextField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+# User Classes
+# Creates options list for UserRating.userclass
+user_classes = ['default', 'recreational', 'medical']
+
+
+# User Ratings Model for online analysis, updating the recommender ranking
+class UserRating(Model):
+    created = DateTimeField(
+        auto_now=True)
+    username = CharField(
+        max_length=100,
+        blank=True,
+        default='defaultuser')
+    userclass = CharField(
+        choices=user_classes,
+        default='default',
+        max_length=100)
+    # Ratings must be scaled down to 1-5 per kaggle dataset original data or
+    #  updated recommendations will be skewed.
+    # These validators may not work in API context.
+    rating = IntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5)
+        ])
+
+    class Meta:
+        ordering = ('created')
+
+
+# Class for storing strain data.
+# Includes both raw text information and embeddings
+class Strains(Model):
+    created = DateTimeField(auto_now=False)
+    updated = DateTimeField(
+        auto_now=True,
+        editable=True)
+    strain_name = CharField(
+        max_length=100,
+        blank=False,
+        editable=True,
+    )
+    # Long form information to be stored as text (huge strings).
+    #   Any request trying to input data to these fields will need to be
+    #   formatted appropriately if using Sqlite3 database.
+    # PostgreSQL offers some advance list-like and json fields, but
+    #   requires setup and connection to external postgreSQL database.
+    strain_effect_list = TextField(blank=False, editable=True)
+    strain_flavor_list = TextField(blank=False, editable=True)
+    strain_desc = TextField(blank=True, editable=True)
+    strain_effect_embed = TextField(blank=True, editable=True)
+    strain_flavor_embed = TextField(blank=True, editable=True)
+    strain_desc_embed = TextField(blank=True, editable=True)
+
+    class Meta:
+        ordered = ('created')
