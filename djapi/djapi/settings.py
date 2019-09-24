fec07e6cdd5817e6251af0f9d00e3a5df829f7bb
newnativeabq <42101276+newnativeabq@users.noreply.github.com>
2019-09-24 14:46:36 -0600

    heroku initial commit

--- djapi/djapi/settings.py
+++ djapi/djapi/settings.py
@@ -153,3 +153,9 @@ LOGGING = {
         },
     },
 }
+
+
+# Configure Django App for Heroku
+import django_heroku
+django_heroku.settings(locals())
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
