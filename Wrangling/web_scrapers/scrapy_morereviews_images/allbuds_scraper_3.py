bd48254c4add415e60aa949c4503f04bb33a76a3
Vincent Brandon <vincenta..brandon@gmail.com>
2019-10-22 14:04:10 +0000

    allbuds image scraper updated/successful

--- Wrangling/web_scrapers/scrapy_morereviews_images/allbuds_scraper_3.py
+++ Wrangling/web_scrapers/scrapy_morereviews_images/allbuds_scraper_3.py
@@ -1,7 +1,12 @@
 """
     Image Downloader
 """
-import urllib3
+import wget
+import os
+import numpy as np
+from time import time, sleep
+from multiprocessing import Process
+
 
 def get_allbud_urls(filename, columns):
     """
@@ -28,15 +33,79 @@ def get_allbud_urls(filename, columns):
 
     return data[columns].tolist()
 
-def parse_name_from_url(url):
+def parse_filename_from_url(url):
+    # Get the strain section from the image URL
     section = url.split('strain/')[-1]
-    filename = section.split('/')[0]
-    return section, filename
-
-http = urllib3.PoolManager()
+    # Take name from section information
+    return url.split('/')[-1]
 
 
-url_list = get_allbud_urls('allbuds_1.csv', 'image_url')
+# Get updated list of urls and features (for image paths, names)
+img_url_list = get_allbud_urls('allbuds_1.csv', 'image_url')
+desc_url_list = get_allbud_urls('allbuds_1.csv', 'desc_url')
 
-for i in range(100):
-    print(parse_name_from_url(url_list[i]))
+# Set Fetch params
+batch_size = 10
+save_path = os.path.join(os.getcwd(), 'images')
+
+
+class Downloader():
+    def __init__(self, batch_size=10, num_threads=6, delay=0.1):
+        self.file_names = [parse_filename_from_url(desc_url) + '.jpg' for desc_url in desc_url_list]
+        self.img_urls = img_url_list
+        self.num_threads = num_threads
+        self.delay = delay
+
+    def download_file(self, url, filepath):
+        """
+        Download file at url to specified filepath
+
+        Parameters
+        ----------
+        url: str
+            url pointing to file
+
+        filepath: str or path object
+            absolute path string or file path of save location
+        """
+        try:
+            wget.download(url, filepath)
+        except:
+            return False
+
+    def create_jobs(self):
+        """
+        Create batches of jobs (list of lists) to map download handler onto
+        """
+        assert len(self.file_names) == len(self.img_urls)
+        self.master_list = np.array(list(zip(self.file_names, self.img_urls)))
+
+        self.jobs = np.array_split(self.master_list, self.num_threads)
+
+    def download_batch(self, batch):
+        for file_params in batch:
+            self.download_file(file_params[1], file_params[0])
+            sleep(self.delay)
+
+    def execute(self):
+        start = time()
+        procs = []
+        for batch in self.jobs:
+            # Create process and store reference
+            proc = Process(target=self.download_batch, args=(batch,))
+            procs.append(proc)
+            # Start process
+            proc.start()
+        # Wait for processes to finish and close
+        for proc in procs:
+            proc.join()
+        stop = time()
+
+        print('{} Jobs completed in {}'.format(len(procs), stop-start))
+
+
+if __name__ == "__main__":
+    downloader = Downloader()
+    downloader.create_jobs()
+    downloader.execute()
+    # print(downloader.jobs)
